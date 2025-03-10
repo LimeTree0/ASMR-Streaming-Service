ec9cc7bb6f0ec2d8f12a64db2834499f1f194fc5
limecoding <bum9516@gmail.com>
2025-03-10 22:43:14 +0900

    Initial commit

---
+++ src/main/java/limecoding/asmrstreamingservice/AsmrStreamingServiceApplication.java
@@ -0,0 +1,13 @@
+package limecoding.asmrstreamingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmrStreamingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsmrStreamingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/limecoding/asmrstreamingservice/AsmrStreamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package limecoding.asmrstreamingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsmrStreamingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
